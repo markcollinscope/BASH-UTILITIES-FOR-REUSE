d8850d805891e682382f9abdb1edbefcff2e9d39
Mark Collins-Cope <markcollinscope@gmail.com>
2021-07-01 07:18:36 +0100

    gitcandp-script-autocommit (PUSHING BRANCH baseline)

---
+++ fndef.sh
@@ -0,0 +1,23 @@
+. utils.shi
+
+USAGE=$(cat <<HERE
+$0 [-n] show a bash function definition
+-n - show fn name only
+HERE
+)
+
+eval $(boolopt -n NAMEONLY $*)
+
+main()
+{
+	setvar PARTFN $1
+
+	RES=$(set | sed -n "/^[[:alnum:]]*$PARTFN.*\s()/,/}/p")
+	if $NAMEONLY; then 
+		RES=$(cat <<< $RES | grep '()') 
+	fi
+	cat <<< $RES	
+}
+# set | grep ".*$1.* ()" | awk '{ print $1; }'
+
+main $*
