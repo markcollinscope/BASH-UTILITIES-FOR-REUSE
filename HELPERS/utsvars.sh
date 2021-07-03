ad0373af39d61744c889666a78e5c2b6590f4e33
Mark Collins-Cope <markcollinscope@gmail.com>
2021-07-03 06:31:42 +0100

    dir-reorganise (PUSHING BRANCH baseline)

---
+++ HELPERS/utsvars.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. utils.shi
+set -u
+set -e
+
+USAGE=$(cat <<ENDUSAGE
+	Usage: $(basename $0)
+	* Print all uts_ vars - recursive descent search used.
+
+ENDUSAGE
+)
+
+
+main()
+{
+	grep -r -h UTS_ | sed 's/.*\(UTS_[[:alnum:]]*\).*/\1/' | sort | uniq
+}
+
+main $*
