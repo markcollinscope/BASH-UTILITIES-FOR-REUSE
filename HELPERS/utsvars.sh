a73806cddedf81ece380fe3f9e86580e4d21a93f
Mark Collins-Cope <markcollinscope@gmail.com>
2021-07-17 05:48:36 +0100

    <baseline+>: chkargcount update

--- HELPERS/utsvars.sh
+++ HELPERS/utsvars.sh
@@ -5,12 +5,14 @@ set -u
 set -e
 
 USAGE=$(cat <<ENDUSAGE
-	Usage: $(basename $0)
-	* Print all uts_ vars - recursive descent search used.
-	* nb: works from *Current* directory downwards.
+
+Usage: $(basename $0)
+=====
+o print all UTS_ vars - recursive descent search used.
+o nb: works from *current* directory downwards.
+
 ENDUSAGE
 )
-
 
 main()
 {
