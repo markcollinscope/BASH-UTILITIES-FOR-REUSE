d3270e7e3611c9e4877c5c3ca608dfc3541fc7f8
Mark Collins-Cope <markcollinscope@gmail.com>
2024-09-15 21:33:24 +0100

    splitopt on binopt _opts working.

--- testopt.sh
+++ testopt.sh
@@ -1,3 +1,4 @@
+
 . utils.shi
 
 _testopt()
@@ -7,6 +8,14 @@ _testopt()
 }
 
 _A=hello
-errecho A: $_A
+errecho 1A: $_A
 _testopt 1  2 3 4 5
+errecho A: $_A
+
+errecho 2A: $_A
+_testopt a b  --aaa c d e f g 
+errecho A: $_A
+
+errecho 3A: $_A
+_testopt -a z y xxx wwwwwww --aaa vvvvvvvvvvvvvvvvvvv -aa uuuuuuuuuuuuuuuuuu t -a
 errecho A: $_A
