1f1c6fb297514407981850ebf8a2f12c6b88dc23
Mark Collins-Cope <markcollinscope@gmail.com>
2024-09-15 22:49:42 +0100

    bopt, boolopt, binopt - wip - including rem

--- testopt.sh
+++ testopt.sh
@@ -4,6 +4,8 @@
 _testopt()
 {
 	eval $(binopt "-a|--aaa" _A false true "$@")
+	eval $(boolopt --rem 'hello world, you all ok?' -b _B "$@")
+	eval $(boolopt  -c:--ccc _C "$@")
 	errecho "$@" 
 }
 
@@ -19,3 +21,11 @@ errecho A: $_A
 errecho 3A: $_A
 _testopt -a z y xxx wwwwwww --aaa vvvvvvvvvvvvvvvvvvv -aa uuuuuuuuuuuuuuuuuu t -a
 errecho A: $_A
+
+errecho 3B: $_B
+_testopt -a z y xxx -b wwwwwww --aaa vvvvvvvvvvvvvvvvvvv -aa uuuuuuuuuuuuuuuuuu t -a
+errecho B: $_B
+
+errecho 3C: $_C
+_testopt 1 2  3 4 5
+errecho C: $_C
